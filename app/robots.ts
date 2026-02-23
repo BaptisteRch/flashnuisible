e0761a7e7f3c09cc7c0aea7b04bcbab54a895f54
BaptisteRch <baptisteeroche@gmail.com>
2026-02-23 23:03:55 +0100

    .com + www.

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://flashnuisible.com/sitemap.xml",
+    sitemap: "https://www.flashnuisible.com/sitemap.xml",
   };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from "next";
 
-const SITE_URL = "https://flashnuisible.com";
+const SITE_URL = "https://www.flashnuisible.com";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const now = new Date();
